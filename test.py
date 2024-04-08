f2364e3e3a0e5cb2200519f8ef7c485c4e1b0b70
yiming huang <yiminghuang@umass.edu>
2024-04-07 20:29:58 -0400

    convert the keylogger to run for duration long

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 # write some test code here
+import threading
+import time
+
 s = "sql "
 command, text = s.split(" ", 1)
 print(command)
 t = ("This", "is", "a", "test")
 print(str(t))
+def test():
+    while True:
+        time.sleep(1)
+t = threading.Thread(target=test)
+t.start()
+t.join(10)

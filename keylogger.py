eaaac4b414a8a634b02098dd790cae59276cbe66
huididin <125756865+huididin@users.noreply.github.com>
2024-04-28 15:30:22 -0400

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,26 +1,26 @@
 import curses
 
-# 初始化 curses
+# initialize curses
 stdscr = curses.initscr()
 curses.noecho()
 stdscr.keypad(True)
 
-# 记录按键事件
+# record keylogger events
 keys = []
 
 try:
     while True:
         key = stdscr.getch()
         keys.append(key)
-        if key == ord('q'):  # 按下 'q' 键退出
+        if key == ord('q'):  # push 'q' to quit
             break
 finally:
-    # 恢复终端设置
+    # revert back to initialized settings
     curses.nocbreak()
     stdscr.keypad(False)
     curses.echo()
     curses.endwin()
 
-# 打印记录的按键
+# print recorded keys
 print(keys)
 

6df50644273961d666edfdd68b20cfe1b02a24f5
huididin <huididing@umass.edu>
2024-05-06 18:54:47 -0400

    Added comments and update Readme

--- screen.py
+++ screen.py
@@ -2,12 +2,19 @@ import tkinter as tk
 import time
 import pyscreenshot as ImageGrab
 
+# Create the main window object using tkinter
 root = tk.Tk()
+# Set the window to fullscreen by changing the attributes of the window
 root.attributes("-fullscreen", True)
 
+# Get the width of the screen using the winfo_screenwidth method of the root window
 screen_width = root.winfo_screenwidth()
+# Get the height of the screen using the winfo_screenheight method of the root window
 screen_height = root.winfo_screenheight()
 
+# Capture the current screen using the grab method from ImageGrab
 screenshot = ImageGrab.grab()
+# Save the captured screenshot to a file named "screenshot.png"
 screenshot.save("screenshot.png")
+# Destroy the root window to close the GUI application
 root.destroy()
--- utils.py
+++ utils.py
@@ -1,14 +1,23 @@
 from stegano import lsb
 
 def stegano_encrypt(datatoencrypt, png):
+      # Embed the data 'datatoencrypt' into the image specified by 'png' using LSB steganography
     secret = lsb.hide(png, datatoencrypt)
+    # Save the new image with the embedded data to a temporary file
     secret.save("/tmp/secret.png")
+    # Open the newly created image file in binary read mode
     with open('/tmp/secret.png', 'rb') as f:
+        # Read the contents of the file into 'data'
         data = f.read()
+         # Return the binary data of the image
     return data
 
 def stegano_decrypt(secretFilePath):
+     # Extract the hidden data from the image file specified by 'secretFilePath' using LSB steganography
     data = lsb.reveal(secretFilePath)
+    # Check if the extracted data is of bytes type
     if isinstance(data, bytes):
+        # Decode the bytes to a string assuming UTF-8 encoding or default string encoding
         data = data.decode()
+         # Return the decoded string or the original data if it wasn't bytes
     return data
